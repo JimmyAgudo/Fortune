76231b280756fdffebad1e2c58f2a7a8fedef52f
JimmyAgudo <44040646+JimmyAgudo@users.noreply.github.com>
2019-02-13 17:40:53 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+$(".Z").click(function() {
+$(".Z").fadeOut();
+$(".f").fadeOut();
+$(".H").fadeOut();
+$(".L").fadeOut();
+$(".K").fadeIn();
+$(".k").fadeIn();
+$(".n").fadeIn();
+$(".N").fadeIn();
+$(".z").fadeIn();
+$(".l").fadeIn();
+$(".p").fadeIn();
+});
+
+$(".p").click(function() {
+$(".Z").fadeOut();
+$(".L").fadeOut();
+$(".K").fadeOut();
+$(".k").fadeOut();
+$(".n").fadeOut();
+$(".N").fadeOut();
+$(".z").fadeOut();
+$(".l").fadeOut();
+$(".p").fadeOut();
+$(".w").fadeIn();
+$(".q").fadeIn();
+});
